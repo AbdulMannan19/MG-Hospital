1e4c80dbec0938d865905f9f68f5f3255bf9e7f3
AbdulMannan19 <abdulmannan34695@gmail.com>
2025-06-23 20:21:40 -0700

    Firebase Set Up

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'Home/home.dart';
 import 'Authentication/login.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Supabase.initialize(
     url: 'https://tuivyasedfkzbkdnbslp.supabase.co',
     anonKey:
