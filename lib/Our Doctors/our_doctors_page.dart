bed0b12a7b5fffe2f32089658d06b359de0eb26b
bariq4 <mirbariq.ali1234@gmail.com>
2025-07-14 04:25:46 +0530

    Fixed overflow issue of our_doctors section

--- lib/Our Doctors/our_doctors_page.dart
+++ lib/Our Doctors/our_doctors_page.dart
@@ -4,6 +4,110 @@ import '../Appointment/appointment_listing.dart';
 class OurDoctorsPage extends StatelessWidget {
   const OurDoctorsPage({super.key});
 
+  // Doctor data moved to a list for maintainability
+  static final List<Map<String, String>> doctors = [
+    {
+      'name': 'Dr. Mohammed Ateeq Ur Rahman',
+      'specialization': 'Consultant Neurology & Stroke Specialist',
+      'imagePath': 'assets/images/doctors/neurology.png',
+    },
+    {
+      'name': 'Dr. Ahmad Abdul Khabeer',
+      'specialization': 'Consultant ENT, Head & Neck, Skull Base Surgeon',
+      'imagePath': 'assets/images/doctors/ent.png',
+    },
+    {
+      'name': 'Dr. Mohd Naqi Zain',
+      'specialization': 'Consultant General & Laparoscopic Surgeon',
+      'imagePath': 'assets/images/doctors/surgery.png',
+    },
+    {
+      'name': 'Dr. Muhammad Azhar Hussain',
+      'specialization': 'Consultant Cardiologist',
+      'imagePath': 'assets/images/doctors/cardiology.png',
+    },
+    {
+      'name': 'Dr. Bushra',
+      'specialization': 'Consultant Paediatrician',
+      'imagePath': 'assets/images/doctors/pediatrics.png',
+    },
+    {
+      'name': 'Dr. Omar Bilal Siddiqui',
+      'specialization': 'Consultant Pulmonology',
+      'imagePath': 'assets/images/doctors/pulmonology.png',
+    },
+    {
+      'name': 'Dr. MD. Yousuf Khan',
+      'specialization': 'Consultant Diabetologist',
+      'imagePath': 'assets/images/doctors/diabetology.png',
+    },
+    {
+      'name': 'Dr. Tahseen Ara Azad',
+      'specialization': 'Consultant Neurocognitive',
+      'imagePath': 'assets/images/doctors/neurology.png',
+    },
+    {
+      'name': 'Dr. MD Tayyab Shaik',
+      'specialization': 'Consultant Physiotherapist',
+      'imagePath': 'assets/images/doctors/physiotherapy.png',
+    },
+    {
+      'name': 'Dr. Quanitha Firdous',
+      'specialization': 'Consultant Physiotherpaist',
+      'imagePath': 'assets/images/doctors/physiotherapy.png',
+    },
+    {
+      'name': 'Dr. G. Hidayathullah',
+      'specialization': 'Consultant Urologist',
+      'imagePath': 'assets/images/doctors/urology.png',
+    },
+    {
+      'name': 'Dr. R. K. Rajesh',
+      'specialization': 'Consultant Urologist',
+      'imagePath': 'assets/images/doctors/urology.png',
+    },
+    {
+      'name': 'Dr. Md. Anwar Ahmed',
+      'specialization': 'Consultant Internal Medicine',
+      'imagePath': 'assets/images/doctors/general-medicine.png',
+    },
+    {
+      'name': 'Dr. Md. Niyaz Ahmed',
+      'specialization': 'Consultant Dermatologist',
+      'imagePath': 'assets/images/doctors/dermatology.png',
+    },
+    {
+      'name': 'Dr. Vazeer Uddin',
+      'specialization': 'Consultant Senior Trauma Surgeon',
+      'imagePath': 'assets/images/doctors/surgery.png',
+    },
+    {
+      'name': 'Dr. Ehsan Ahmed Khan',
+      'specialization': 'Consultant Senior Physician',
+      'imagePath': 'assets/images/doctors/general-medicine.png',
+    },
+    {
+      'name': 'Dr. Mohammed Yaseen',
+      'specialization': 'Consultant Paediatrician & Neonatology',
+      'imagePath': 'assets/images/doctors/pediatrics.png',
+    },
+    {
+      'name': 'Dr. Saba Samreen',
+      'specialization': 'Consultant Obstetrics & Gynaecologist',
+      'imagePath': 'assets/images/doctors/gynecology.png',
+    },
+    {
+      'name': 'Dr. Fouzia Jeelani',
+      'specialization': 'Senior Consultant Obstetrics & Gynaecologist',
+      'imagePath': 'assets/images/doctors/gynecology.png',
+    },
+    {
+      'name': 'Dr. Fatma Mohammed',
+      'specialization': 'Consultant Obstetrician & Gynaecologist',
+      'imagePath': 'assets/images/doctors/gynecology.png',
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,135 +153,39 @@ class OurDoctorsPage extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.all(16.0),
-              child: GridView.count(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                crossAxisCount: 2,
-                mainAxisSpacing: 16,
-                crossAxisSpacing: 16,
-                childAspectRatio: 0.75,
-                children: [
-                  _buildDoctorCard(
-                    context,
-                    'Dr. Mohammed Ateeq Ur Rahman',
-                    'Consultant Neurology & Stroke Specialist',
-                    'assets/images/doctors/neurology.png',
-                  ),
-                  _buildDoctorCard(
-                    context,
-                    'Dr. Ahmad Abdul Khabeer',
-                    'Consultant ENT, Head & Neck, Skull Base Surgeon',
-                    'assets/images/doctors/ent.png',
-                  ),
-                  _buildDoctorCard(
-                    context,
-                    'Dr. Mohd Naqi Zain',
-                    'Consultant General & Laparoscopic Surgeon',
-                    'assets/images/doctors/surgery.png',
-                  ),
-                  _buildDoctorCard(
-                    context,
-                    'Dr. Muhammad Azhar Hussain',
-                    'Consultant Cardiologist',
-                    'assets/images/doctors/cardiology.png',
-                  ),
-                  _buildDoctorCard(
-                    context,
-                    'Dr. Bushra',
-                    'Consultant Paediatrician',
-                    'assets/images/doctors/pediatrics.png',
-                  ),
-                  _buildDoctorCard(
-                    context,
-                    'Dr. Omar Bilal Siddiqui',
-                    'Consultant Pulmonology',
-                    'assets/images/doctors/pulmonology.png',
-                  ),
-                  _buildDoctorCard(
-                    context,
-                    'Dr. MD. Yousuf Khan',
-                    'Consultant Diabetologist',
-                    'assets/images/doctors/diabetology.png',
-                  ),
-                  _buildDoctorCard(
-                    context,
-                    'Dr. Tahseen Ara Azad',
-                    'Consultant Neurocognitive',
-                    'assets/images/doctors/neurology.png',
-                  ),
-                  _buildDoctorCard(
-                    context,
-                    'Dr. MD Tayyab Shaik',
-                    'Consultant Physiotherapist',
-                    'assets/images/doctors/physiotherapy.png',
-                  ),
-                  _buildDoctorCard(
-                    context,
-                    'Dr. Quanitha Firdous',
-                    'Consultant Physiotherpaist',
-                    'assets/images/doctors/physiotherapy.png',
-                  ),
-                  _buildDoctorCard(
-                    context,
-                    'Dr. G. Hidayathullah',
-                    'Consultant Urologist',
-                    'assets/images/doctors/urology.png',
-                  ),
-                  _buildDoctorCard(
-                    context,
-                    'Dr. R. K. Rajesh',
-                    'Consultant Urologist',
-                    'assets/images/doctors/urology.png',
-                  ),
-                  _buildDoctorCard(
-                    context,
-                    'Dr. Md. Anwar Ahmed',
-                    'Consultant Internal Medicine',
-                    'assets/images/doctors/general-medicine.png',
-                  ),
-                  _buildDoctorCard(
-                    context,
-                    'Dr. Md. Niyaz Ahmed',
-                    'Consultant Dermatologist',
-                    'assets/images/doctors/dermatology.png',
-                  ),
-                  _buildDoctorCard(
-                    context,
-                    'Dr. Vazeer Uddin',
-                    'Consultant Senior Trauma Surgeon',
-                    'assets/images/doctors/surgery.png',
-                  ),
-                  _buildDoctorCard(
-                    context,
-                    'Dr. Ehsan Ahmed Khan',
-                    'Consultant Senior Physician',
-                    'assets/images/doctors/general-medicine.png',
-                  ),
-                  _buildDoctorCard(
-                    context,
-                    'Dr. Mohammed Yaseen',
-                    'Consultant Paediatrician & Neonatology',
-                    'assets/images/doctors/pediatrics.png',
-                  ),
-                  _buildDoctorCard(
-                    context,
-                    'Dr. Saba Samreen',
-                    'Consultant Obstetrics & Gynaecologist',
-                    'assets/images/doctors/gynecology.png',
-                  ),
-                  _buildDoctorCard(
-                    context,
-                    'Dr. Fouzia Jeelani',
-                    'Senior Consultant Obstetrics & Gynaecologist',
-                    'assets/images/doctors/gynecology.png',
-                  ),
-                  _buildDoctorCard(
-                    context,
-                    'Dr. Fatma Mohammed',
-                    'Consultant Obstetrician & Gynaecologist',
-                    'assets/images/doctors/gynecology.png',
-                  ),
-                ],
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  int crossAxisCount = constraints.maxWidth > 900
+                      ? 4
+                      : constraints.maxWidth > 600
+                          ? 3
+                          : 2;
+                  double childAspectRatio = constraints.maxWidth > 900
+                      ? 0.75
+                      : constraints.maxWidth > 600
+                          ? 0.7
+                          : 0.65;
+                  return GridView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: crossAxisCount,
+                      mainAxisSpacing: 16,
+                      crossAxisSpacing: 16,
+                      childAspectRatio: childAspectRatio,
+                    ),
+                    itemCount: doctors.length,
+                    itemBuilder: (context, index) {
+                      final doctor = doctors[index];
+                      return _buildDoctorCard(
+                        context,
+                        doctor['name']!,
+                        doctor['specialization']!,
+                        doctor['imagePath']!,
+                      );
+                    },
+                  );
+                },
               ),
             ),
           ],
@@ -200,48 +208,54 @@ class OurDoctorsPage extends StatelessWidget {
             borderRadius: const BorderRadius.vertical(top: Radius.circular(12)),
             child: Image.asset(
               imagePath,
-              height: 160,
+              height: 110, // Reduced height for better fit
               width: double.infinity,
               fit: BoxFit.cover,
               errorBuilder: (context, error, stackTrace) {
                 return Container(
-                  height: 160,
+                  height: 110,
                   color: const Color(0xFF13a8b4),
                   child: const Icon(
                     Icons.person,
-                    size: 80,
+                    size: 60,
                     color: Colors.white,
                   ),
                 );
               },
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xFF13a8b4),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Flexible(
+                    child: Text(
+                      name,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF13a8b4),
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
                   ),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                const SizedBox(height: 4),
-                Text(
-                  specialization,
-                  style: const TextStyle(
-                    fontSize: 12,
-                    color: Colors.black87,
+                  const SizedBox(height: 4),
+                  Flexible(
+                    child: Text(
+                      specialization,
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: Colors.black87,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
                   ),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
